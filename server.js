ac47e38711502e18440114d108a69f756a7d4e0e
Chris Zuniga <zunigachristian81@gmail.com>
2021-11-06 17:25:54 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const db = require("./develop/db/db.json");
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("listening to PORT " + PORT);
+});
